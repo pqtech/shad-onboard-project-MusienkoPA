9e88dff7d3854c43a76eefb2e8f094fe1d579548
pqtech <p.musienko@gmail.com>
2021-02-03 19:08:53 +0300

    Add BeforeSuite with unpacking method

--- compareXml/src/test/java/CompareXmlFilesTest.java
+++ compareXml/src/test/java/CompareXmlFilesTest.java
@@ -1,5 +1,7 @@
+import org.testng.annotations.BeforeSuite;
 import org.testng.annotations.Test;
 import services.ProcessXmlFile;
+import services.ProcessZipFile;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.xmlunit.matchers.CompareMatcher.isIdenticalTo;
@@ -9,6 +11,15 @@ public class CompareXmlFilesTest {
     public static final String PATH_TO_FILE_A = "src/data/gold_data/A.xml";
     public static final String PATH_TO_FILE_B = "src/data/output_files/B.xml";
     public static final String TAG_TO_IGNORE_TEXT = "cite.query";
+    private static final String PATH_TO_ZIP_FILE = "src/Files.zip";
+    private static final String PATH_TO_GOLD_DATA_FOLDER = "src/data/gold_data/";
+    private static final String PATH_TO_OUTPUT_FILES_FOLDER = "src/data/output_files/";
+
+
+    @BeforeSuite
+    public void setUp() {
+        ProcessZipFile.unpackFilesAB(PATH_TO_ZIP_FILE, PATH_TO_GOLD_DATA_FOLDER, PATH_TO_OUTPUT_FILES_FOLDER);
+    }
 
     @Test
     public void compareTwoXmlFilesTest() {
