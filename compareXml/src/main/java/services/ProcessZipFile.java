0d8db23e37060f1f50d65799e305e22ba864030f
pqtech <p.musienko@gmail.com>
2021-02-05 00:45:34 +0300

    Separate loggers for errors and debug messages

--- compareXml/src/main/java/services/ProcessZipFile.java
+++ compareXml/src/main/java/services/ProcessZipFile.java
@@ -5,13 +5,14 @@ import net.lingala.zip4j.exception.ZipException;
 import org.apache.log4j.Logger;
 
 public class ProcessZipFile {
-    final static Logger logger = Logger.getLogger(ProcessXmlFile.class);
+    final static Logger loggerDebug = Logger.getLogger("debugLogFile");
+    final static Logger loggerError = Logger.getLogger("errorLogFile");
 
     public static void unpackFilesAB(String pathToZipFile,
                                      String pathToGoldDataFolder,
                                      String pathToOutputFilesFolder) {
         ZipFile zipFile = new ZipFile(pathToZipFile);
-        logger.debug("Current zip file is valid: " + zipFile.isValidZipFile());
+        loggerDebug.debug("Current zip file is valid: " + zipFile.isValidZipFile());
 
         try {
             // Sorting and unpacking A/B xml files
@@ -25,14 +26,12 @@ public class ProcessZipFile {
                     zipFile.extractFile(currentFileName, pathToOutputFilesFolder,
                             currentFileName.replaceAll("Files/", ""));
                 } else {
-                    logger.debug("Incorrect file name for unpacking: " + currentFileName);
+                    loggerDebug.debug("Incorrect file name for unpacking: " + currentFileName);
                 }
             }
         } catch (ZipException e) {
             e.printStackTrace();
-            logger.error("Something wrong. Error text: " + e);
-
+            loggerError.error("Something's wrong. Error text: " + e);
         }
-
     }
 }
