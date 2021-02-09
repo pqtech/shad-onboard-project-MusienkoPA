fbb62c3df2dd9701e50c3437d2f10afb7031278c
pqtech <p.musienko@gmail.com>
2021-02-09 13:53:42 +0300

    Add custom DifferenceEvaluator. Add file names output to Assert messages.

--- compareXml/src/test/java/CompareXmlFilesTest.java
+++ compareXml/src/test/java/CompareXmlFilesTest.java
@@ -1,47 +1,43 @@
-import org.apache.log4j.Logger;
-import org.testng.Assert;
+import CustomDifferenceEvaluators.DifferenceEvaluatorWithLogger;
 import org.testng.annotations.BeforeSuite;
 import org.testng.annotations.Test;
 import org.xmlunit.builder.DiffBuilder;
 import org.xmlunit.diff.Diff;
-import org.xmlunit.diff.Difference;
-import services.ProcessXmlFile;
-import services.ProcessZipFile;
+import utils.ProcessXmlFile;
+import utils.ProcessZipFile;
+
+import static org.testng.Assert.assertFalse;
 
 public class CompareXmlFilesTest {
-    static final Logger loggerError = Logger.getLogger("errorLogFile");
-
     private static final String TAG_TO_IGNORE = "cite.query";
     private static final String PATH_TO_ZIP_FILE = "../Files.zip";
+    private static final String SORT_REGEX_FILE_A = ".*?A[\\d]*.xml";
+    private static final String SORT_REGEX_FILE_B = ".*?B[\\d]*.xml";
 
     @BeforeSuite
     public void setUp() {
         ProcessZipFile.unpackFilesAB(PATH_TO_ZIP_FILE,
                 DataProviders.PATH_TO_GOLD_DATA_FOLDER,
-                DataProviders.PATH_TO_OUTPUT_FILES_FOLDER);
+                DataProviders.PATH_TO_OUTPUT_FILES_FOLDER,
+                SORT_REGEX_FILE_A,
+                SORT_REGEX_FILE_B);
     }
 
     @Test(dataProviderClass = DataProviders.class, dataProvider = "A/B xml files")
-    public void compareTwoXmlFilesTest(String pathToFileA, String pathToFileB) {
+    public void compareTwoXmlFilesTest(String nameFileA, String nameFileB) {
+        String pathToFileA = DataProviders.PATH_TO_GOLD_DATA_FOLDER + nameFileA;
+        String pathToFileB = DataProviders.PATH_TO_OUTPUT_FILES_FOLDER + nameFileB;
+
         // Comparing two XML files
         Diff diff = DiffBuilder.compare(ProcessXmlFile
                 .getProcessedXmlFile(pathToFileA, TAG_TO_IGNORE))
                 .withTest(ProcessXmlFile.getProcessedXmlFile(pathToFileB, TAG_TO_IGNORE))
+                .withDifferenceEvaluator(new DifferenceEvaluatorWithLogger(pathToFileA, pathToFileB, TAG_TO_IGNORE))
                 .ignoreWhitespace()
                 .checkForIdentical()
                 .build();
 
-        // Logging differences after replacing original tags back
-        int currentDiff = 0;
-        for (Difference d : diff.getDifferences()) {
-            currentDiff++;
-            loggerError.error("File [" + pathToFileA + "] and file [" + pathToFileB
-                    + "], difference #"
-                    + currentDiff + ":\n" + d.toString()
-                    .replaceAll("\\[" + TAG_TO_IGNORE, "<" + TAG_TO_IGNORE)
-                    .replaceAll("\\[/" + TAG_TO_IGNORE + "\\]", "</" + TAG_TO_IGNORE + ">"));
-        }
-
-        Assert.assertFalse(diff.hasDifferences(), "Check /log/error.log file.");
+        assertFalse(diff.hasDifferences(), "There are differences between file "
+                + nameFileA + " and " + nameFileB + ". Check /log/error.log file for details.");
     }
 }
